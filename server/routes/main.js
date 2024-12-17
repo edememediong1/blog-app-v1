db95a87287d75fcf1cf6ab15db01ad46221cf984
Emediong Bassey Edem <edememediong1@gmail.com>
2024-12-18 00:34:49 +0100

    configuring the pths

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,9 +4,12 @@ const router = express.Router();
 
 //Routes
 router.get("", (req, res) => {
+    const locals= {
+        title : "Nodejs Blog",
+        description: "Simple Blog created with Nodejs"
+    }
 
-        
-    res.render("index")
+    res.render("index", {locals})
 })
 
 router.get("/about", (req, res)=> {
