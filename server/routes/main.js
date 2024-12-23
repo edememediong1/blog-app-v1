177b8d9d97c63055c67d335c0d6b24b2c984cfab
Emediong Bassey Edem <edememediong1@gmail.com>
2024-12-23 10:23:14 +0100

    adding pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -9,14 +9,31 @@ const Post = require("../models/Post")
     HOME
 */
 router.get("", async (req, res) => {
-    const locals= {
-        title : "Nodejs Blog",
-        description: "Simple Blog created with Nodejs"
-    }
 
     try {
-        const data = await Post.find();
-        res.render("index", {locals, data})
+        const locals = {
+            title: "Nodejs Blog",
+            description: "Welcome to my blog"
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{ $sort : { createdAt: -1}}])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage)
+        
+        res.render("index", {
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        })
     } catch (error) {
         console.log(error)
     }
